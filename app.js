53a176fd2e8ddfbce4508e5d24c60f9a872daa4a
gary <gary.white@lodgit.com>
2022-06-23 13:32:18 +0200

    code

---
+++ app.js
@@ -0,0 +1,6 @@
+function chromosomeCheck(sperm) {
+
+}
+
+console.log(chromosomeCheck('XY')); // "Congratulations! You're going to have a son."
+console.log(chromosomeCheck('XX')); // "Congratulations! You're going to have a daughter."
