d07605d3cba88f6e927730623852ca9ef712f65b
gary <gary.white@lodgit.com>
2022-06-23 13:35:56 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function chromosomeCheck(sperm) {
-
+    if (sperm.includes('XY')) {
+        return "Congratulations! You're going to have a son."
+    } else {
+        return "Congratulations! You're going to have a daughter."
+    }
 }
 
 console.log(chromosomeCheck('XY')); // "Congratulations! You're going to have a son."
